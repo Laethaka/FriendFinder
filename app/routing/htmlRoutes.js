37346318b612153db01b279bbab70855f55d7dfd
Jesse <laethaka@gmail.com>
2018-05-31 13:59:06 -0700

    first path works but not with server.js

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var PORT = 5005;
+
+app.get('/', function(req,res) {
+    res.sendFile(path.join(__dirname + '/../public', "home.html"));
+});
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
